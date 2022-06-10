d6400529d4170508de33d2c91f41d52831e958bd
amir <amir@gmail.com>
2022-06-10 19:42:51 +0430

    vue 3 added successfully

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import welcome from "./components/welcome";
+
+const app = createApp({});
+
+app.component('welcome' , welcome);
+app.mount('#app');
+
+
---
+++ resources/js/components/welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>salam {{ name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    setup: ()=>({
+        name: 'mammad'
+    })
+}
+</script>
+
+<style>
+
+</style>
